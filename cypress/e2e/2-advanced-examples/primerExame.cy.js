031a4f9253bafea87a23025a90bf871c557a4c7f
JvrChavez <ariel.1999@outlook.com>
2022-09-12 20:59:17 -0700

    Avance del examen de la tarde
    Aun falta, nos cambiamos a la pagina de cyberpuerta, trataremos de hacerlo funcionar

--- cypress/e2e/2-advanced-examples/primerExame.cy.js
+++ cypress/e2e/2-advanced-examples/primerExame.cy.js
@@ -3,16 +3,24 @@
 describe('Tercer feature de acass avanzados',function(){
     this.beforeEach(()=>{
         //ingresar a la pagina de compra de articulos tecnologicos
-        cy.visit("https://www.telcel.com/")
+        cy.visit("https://www.cyberpuerta.com")
     })
     //Ingresar a tienda online
     it('Realizar compra de celular iPhone13',function(){
-        //cy.get('.estado > .dropdown-menu > .dropdown-content > :nth-child(1) > a').click({force:true})
-        cy.get(':nth-child(3) > .black-mobile').click({force:true})
+        /*Telcel
         cy.get('tbody > :nth-child(1) > :nth-child(1) > a > img').click()
         cy.get('.chosen-single').type("sonora")
-        cy.get('#entrarPerfilador').click({force:true})
-        //cy.get('div[class="chosen-drop"').contains('label',this.datos.hobbies[0]).parent().find('input').check({force:true})
+        cy.get('.active-result').should('contain.text','Sonora').click()
+        cy.get('#entrarPerfilador').click({force:true})*/
+        
+        //Cyberpuerta
+        cy.get('[href="/Computadoras/"]').click()
+        cy.get('[href="/Computadoras/All-in-One/"]').click()
+        cy.get(':nth-child(1) > .cp-pf-cklist > ul > :nth-child(1) > label > span').click()
+        cy.get(':nth-child(4) > .cp-pf-cklist > ul > :nth-child(1) > label > span').click()
+        cy.get('.emdropdownicon').click()
+        //cy.get('div[class^="emdropdownitems"]').contains('label','Menor precio').click()
+
 
 
     })
