249ac4d36cddee834dfdba3ec751016c619a6956
JvrChavez <ariel.1999@outlook.com>
2022-09-13 01:44:35 -0700

    Detallitos despues de examen
    Se cambiaron detalles minimos de etiquetas

--- cypress/e2e/2-advanced-examples/primerExame.cy.js
+++ cypress/e2e/2-advanced-examples/primerExame.cy.js
@@ -1,13 +1,12 @@
 /// <reference types="Cypress"/>
-//Suite de casos de pruebas avanzadas
+//Suite de Primer Examen
 describe('Primer Examen',function(){
     this.beforeEach(()=>{
         //Ingresar a la pagina de compra de articulos tecnologicos
         cy.visit("https://www.cyberpuerta.com")
     })
-    //Ingresar a tienda online
+    
     it('Realizar compra de una computadora Apple',function(){
-        
         //Cyberpuerta
         //Entramos al area de computadoras All in One
         cy.get('[href="/Computadoras/"]').click()
@@ -27,9 +26,6 @@ describe('Primer Examen',function(){
 
         //Verificamos que solo tengamos un producto en el carrito
         cy.get('.basketboxcount').should('contain.text','1')
-
-
-
     })
 })
 Cypress.on('uncaught:exception',(err,runnable)=>{
