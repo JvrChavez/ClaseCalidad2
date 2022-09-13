6907841b6fdafdf130a0e7242bf0a20fac9e8785
JvrChavez <ariel.1999@outlook.com>
2022-09-13 01:08:16 -0700

    Examen realizado
    Se termino el Primer Examen usando la pagina de Cyberpuerta

--- cypress/e2e/2-advanced-examples/primerExame.cy.js
+++ cypress/e2e/2-advanced-examples/primerExame.cy.js
@@ -1,25 +1,32 @@
 /// <reference types="Cypress"/>
 //Suite de casos de pruebas avanzadas
-describe('Tercer feature de acass avanzados',function(){
+describe('Primer Examen',function(){
     this.beforeEach(()=>{
-        //ingresar a la pagina de compra de articulos tecnologicos
+        //Ingresar a la pagina de compra de articulos tecnologicos
         cy.visit("https://www.cyberpuerta.com")
     })
     //Ingresar a tienda online
-    it('Realizar compra de celular iPhone13',function(){
-        /*Telcel
-        cy.get('tbody > :nth-child(1) > :nth-child(1) > a > img').click()
-        cy.get('.chosen-single').type("sonora")
-        cy.get('.active-result').should('contain.text','Sonora').click()
-        cy.get('#entrarPerfilador').click({force:true})*/
+    it('Realizar compra de una computadora Apple',function(){
         
         //Cyberpuerta
+        //Entramos al area de computadoras All in One
         cy.get('[href="/Computadoras/"]').click()
         cy.get('[href="/Computadoras/All-in-One/"]').click()
-        cy.get(':nth-child(1) > .cp-pf-cklist > ul > :nth-child(1) > label > span').click()
-        cy.get(':nth-child(4) > .cp-pf-cklist > ul > :nth-child(1) > label > span').click()
-        cy.get('.emdropdownicon').click()
-        //cy.get('div[class^="emdropdownitems"]').contains('label','Menor precio').click()
+
+        //Filtramos productos en existencia de la marca Apple
+        cy.get('div[class^="cp-pf-cklist"').contains('label','En existencia').parent().find('input').check({force:true})
+        cy.get('div[class^="cp-pf-cklist"').contains('label','Apple').parent().find('input').check({force:true})
+
+        //Verificamos que el producto sea Apple
+        cy.get('#productList-2').should('contain.text','Apple').click()
+        cy.get('.detailsInfo_right_title').should('contain.text','Apple')
+
+        //Agregamos al carrito y vamos a este mismo
+        cy.get('.basketButton').click()
+        cy.get('.oxwidget_headerminibasket_tobasket').click()
+
+        //Verificamos que solo tengamos un producto en el carrito
+        cy.get('.basketboxcount').should('contain.text','1')
 
 
 
