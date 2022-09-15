be2b559d5c70b50896bae61fad2590e88d531672
JvrChavez <ariel.1999@outlook.com>
2022-09-15 13:50:32 -0700

    Clase 15 Sep
    Automatizacion en lista

--- cypress/e2e/2-advanced-examples/PruebasAvanzadas.cy.js
+++ cypress/e2e/2-advanced-examples/PruebasAvanzadas.cy.js
@@ -1,6 +1,12 @@
 /// <reference types="Cypress"/>
 //Suite de casos de pruebas avanzadas
 describe('Tercer feature de acass avanzados',function(){
+    before(function(){
+        //Cargamos los valores del archivo example.json en un objeto de datos
+        cy.fixture('carritoDeCompras').then(function(datos){
+            this.datos=datos
+        })
+    })
     this.beforeEach(()=>{
         //ingresar a la pagina de compra de articulos tecnologicos
         cy.visit("https://demo.opencart.com/index.php")
@@ -10,16 +16,25 @@ describe('Tercer feature de acass avanzados',function(){
         cy.get("#menu ul a:contains('Phones & PDAs')").click()
         cy.get("div[class='product-thumb']").as("contenedorDeProductos")
         cy.get("@contenedorDeProductos")
-        cy.get('div[class="product-thumb"]:has(.description):contains(HTC Touch HD)')
+        cy.get('div[class="product-thumb"]:has(.description)/*:contains(HTC Touch HD)')
         .each(($el, index, $list) => {
-            cy.get('.has(.caption) h4 a').eq(index).find('.price').then(function($el1){
+            cy.get(':contains(HTC Touch HD)').eq(index).then(function($el1){
                 let producto = $el1.text()
                 cy.log(producto)
 
-                if(producto.includes('HTC Touch HD')){
-                    cy.log('Se ha encontrado el elemento buscado')
-                    cy.get('@ContenedorDePruductos').eq(index).find('button[onclick^="cart.add"]').click()
+                if(producto.includes(this.datos.telefono1)){
+                    cy.log('Se ha encontrado el elemento buscado '+this.datos.telefono1)
+                    cy.get('@contenedorDeProductos').eq(index)/*.find('button[onclick^="cart.add"]')*/.click()
                 }
+                if(producto.includes(this.datos.telefono2)){
+                    cy.log('Se ha encontrado el elemento buscado'+this.datos.telefono2)
+                    cy.get('@contenedorDeProductos').eq(index)/*.find('button[onclick^="cart.add"]')*/.click()
+                }
+                if(producto.includes(this.datos.telefono3)){
+                    cy.log('Se ha encontrado el elemento buscado'+this.datos.telefono3)
+                    cy.get('@contenedorDeProductos').eq(index)/*.find('button[onclick^="cart.add"]')*/.click()
+                }
+
             })
         })
     })
--- cypress/e2e/2-advanced-examples/primerExame.cy.js
+++ cypress/e2e/2-advanced-examples/primerExame.cy.js
@@ -13,10 +13,32 @@ describe('Primer Examen',function(){
         cy.get('[href="/Computadoras/All-in-One/"]').click()
 
         //Filtramos productos en existencia de la marca Apple
+        cy.wait(3000)
+        cy.get('div[class^="cp-pf-cklist"').contains('label','APPLE').parent().find('input').check({force:true})
         cy.get('div[class^="cp-pf-cklist"').contains('label','En existencia').parent().find('input').check({force:true})
-        cy.get('div[class^="cp-pf-cklist"').contains('label','Apple').parent().find('input').check({force:true})
+        
+        cy.get('.emproduct')
+        cy.get('.emproduct').as("contenedorDeProductos")
+        cy.get("@contenedorDeProductos")
+        cy.get('div[class="emproduct_right"]')
+        .each(($el, index, $list) => {
+            cy.get('a[class^="emproduct_right_title"]').eq(index).then(function($el1){
+                let producto = $el1.text()
+                cy.log(producto)
 
-        //Verificamos que el producto sea Apple
+                if(producto.includes(this.datos.PC1)){
+                    cy.log('Se ha encontrado el elemento buscado '+this.datos.telefono1)
+                    cy.get('@contenedorDeProductos').eq(index)/*.find('button[onclick^="cart.add"]')*/.click()
+                }
+                if(producto.includes(this.datos.PC2)){
+                    cy.log('Se ha encontrado el elemento buscado'+this.datos.telefono2)
+                    cy.get('@contenedorDeProductos').eq(index)/*.find('button[onclick^="cart.add"]')*/.click()
+                }
+
+            })
+        })
+
+        /*//Verificamos que el producto sea Apple
         cy.get('#productList-2').should('contain.text','Apple').click()
         cy.get('.detailsInfo_right_title').should('contain.text','Apple')
 
@@ -25,7 +47,7 @@ describe('Primer Examen',function(){
         cy.get('.oxwidget_headerminibasket_tobasket').click()
 
         //Verificamos que solo tengamos un producto en el carrito
-        cy.get('.basketboxcount').should('contain.text','1')
+        cy.get('.basketboxcount').should('contain.text','1')*/
     })
 })
 Cypress.on('uncaught:exception',(err,runnable)=>{
